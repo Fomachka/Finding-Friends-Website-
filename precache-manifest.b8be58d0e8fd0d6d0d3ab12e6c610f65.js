72d5b220db72282f31a982f0dd2319eb0aa5d615
Fomachka <fomachka93@yahoo.com>
2019-12-26 15:29:59 +0900

    Updates

---
+++ precache-manifest.b8be58d0e8fd0d6d0d3ab12e6c610f65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c28b886ff307f68cf666f889a919590",
+    "url": "/Friendly-Search-Engine/index.html"
+  },
+  {
+    "revision": "afe37d8cb1a0c689aac6",
+    "url": "/Friendly-Search-Engine/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fbd717a3a8eadb54c429",
+    "url": "/Friendly-Search-Engine/static/css/main.dfb12498.chunk.css"
+  },
+  {
+    "revision": "afe37d8cb1a0c689aac6",
+    "url": "/Friendly-Search-Engine/static/js/2.5907de8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Friendly-Search-Engine/static/js/2.5907de8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbd717a3a8eadb54c429",
+    "url": "/Friendly-Search-Engine/static/js/main.76c9700e.chunk.js"
+  },
+  {
+    "revision": "bdbd852ec368260be1b9",
+    "url": "/Friendly-Search-Engine/static/js/runtime-main.dca7d9d8.js"
+  },
+  {
+    "revision": "2cc2be525a94fcea4c81af8d0cbf2200",
+    "url": "/Friendly-Search-Engine/static/media/No. Seven Regular.2cc2be52.woff"
+  }
+]);
